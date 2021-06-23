fb75bde7b0e7b84bfc5bad784d9bd86df930d79a
Slimane <aitslim@gmail.com>
2021-06-23 11:33:22 +0200

    Page artistes

---
+++ src/Entity/Artiste.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArtisteRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ArtisteRepository::class)
+ */
+class Artiste
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=150)
+     */
+    private $NomArtiste;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Category::class, inversedBy="artistes")
+     */
+    private $category;
+
+    public function __construct()
+    {
+        $this->category = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomArtiste(): ?string
+    {
+        return $this->NomArtiste;
+    }
+
+    public function setNomArtiste(string $NomArtiste): self
+    {
+        $this->NomArtiste = $NomArtiste;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Category[]
+     */
+    public function getCategory(): Collection
+    {
+        return $this->category;
+    }
+
+    public function addCategory(Category $category): self
+    {
+        if (!$this->category->contains($category)) {
+            $this->category[] = $category;
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(Category $category): self
+    {
+        $this->category->removeElement($category);
+
+        return $this;
+    }
+}
---
+++ src/Entity/Category.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CategoryRepository::class)
+ */
+class Category
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $nameCategory;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Artiste::class, mappedBy="category")
+     */
+    private $artistes;
+
+    public function __construct()
+    {
+        $this->artistes = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNameCategory(): ?string
+    {
+        return $this->nameCategory;
+    }
+
+    public function setNameCategory(string $nameCategory): self
+    {
+        $this->nameCategory = $nameCategory;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Artiste[]
+     */
+    public function getArtistes(): Collection
+    {
+        return $this->artistes;
+    }
+
+    public function addArtiste(Artiste $artiste): self
+    {
+        if (!$this->artistes->contains($artiste)) {
+            $this->artistes[] = $artiste;
+            $artiste->addCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeArtiste(Artiste $artiste): self
+    {
+        if ($this->artistes->removeElement($artiste)) {
+            $artiste->removeCategory($this);
+        }
+
+        return $this;
+    }
+}
