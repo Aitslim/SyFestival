cc129cfbbd9e54bb07c8bef78d9491f6d649209a
Slimane <aitslim@gmail.com>
2021-06-23 18:52:58 +0200

    Menu category with color

--- src/Controller/ArtisteController.php
+++ src/Controller/ArtisteController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ArtistRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,13 +13,15 @@ class ArtisteController extends AbstractController
     /**
      * @Route("/artistes", name="artistes")
      */
-    public function index(ArtistRepository $artistsRepository): Response
+    public function index(ArtistRepository $artistsRepository, CategoryRepository $categoryRepository): Response
     {
-        $artists = $artistsRepository->findAll();
+        $artistes = $artistsRepository->findAll();
+        $categories = $categoryRepository->findAll();
         // dd($artists);
 
         return $this->render('artiste/artistes.html.twig', [
-            'artistes' => $artists,
+            'artistes' => $artistes,
+            'categories' => $categories
         ]);
     }
 }
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -25,6 +25,11 @@ class Category
     private $name;
 
     /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $color;
+
+    /**
      * @ORM\OneToMany(targetEntity=Artist::class, mappedBy="category")
      */
     private $artists;
@@ -47,6 +52,17 @@ class Category
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color): self
+    {
+        $this->color = $color;
 
         return $this;
     }
